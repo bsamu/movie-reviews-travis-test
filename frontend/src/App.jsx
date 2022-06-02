8feb85a7cd84b1c491ab5494414f4094712c3361
agolautner <lautner.agoston@gmail.com>
2022-06-02 17:21:51 +0200

    add loggedIn State

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route, useSearchParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home"
 import Review from "./components/Review";
 import Login from "./components/Login";
 import Movies from "./components/Movies";
@@ -9,25 +10,22 @@ import ReactPaginate from "react-paginate";
 
 const axios = require("axios");
 
-const Home = () => {
-  return (
-    <div>
-      <h2>HOME</h2>
-      <p>Welcome to the Home section!</p>
-    </div>
-  );
-};
-
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if ((sessionStorage.getItem('token')) && !loggedIn) setLoggedIn(true);
+  })
+
   return (
     <div className="App">
+      {loggedIn && <p>logged in!</p>}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reviews" element={<Review />} />
         <Route path="/callback" element={<Login />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="/reviews" element={<Review />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <div>
+        <h2>HOME</h2>
+        <p>Welcome to the Home section!</p>
+      </div>
+    );
+  };
+
+  export default Home
