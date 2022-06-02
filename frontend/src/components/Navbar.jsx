3e7bea8922fe7ed33cb10a464b6584106009cb39
kordoran <andorkorcsog@gmail.com>
2022-06-02 17:20:18 +0200

    Navbar.scss created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import "./Navbar.scss";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="navbar">
       <button
+        className="google-btn"
         onClick={() =>
           window.open(
             `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=97294605994-q0nsjfmme8dn208jkaeu3u8t3ik4am1b.apps.googleusercontent.com&scope=openid%20email&redirect_uri=http://localhost:3000/callback`
@@ -13,9 +15,35 @@ const Navbar = () => {
       >
         Log in with Google
       </button>
-      <Link to="/">Home</Link>
-      <Link to="/movies">Movies</Link>
-      <Link to="/reviews">My reviews</Link>
+      <nav>
+        <NavLink
+          to="/"
+          style={({ isActive }) => ({
+            fontWeight: isActive ? "400" : "300",
+            borderBottom: isActive ? "2px solid" : "none",
+          })}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/movies"
+          style={({ isActive }) => ({
+            fontWeight: isActive ? "400" : "300",
+            borderBottom: isActive ? "2px solid" : "none",
+          })}
+        >
+          Movies
+        </NavLink>
+        <NavLink
+          to="/reviews"
+          style={({ isActive }) => ({
+            fontWeight: isActive ? "400" : "300",
+            borderBottom: isActive ? "2px solid" : "none",
+          })}
+        >
+          My reviews
+        </NavLink>
+      </nav>
     </div>
   );
 };
